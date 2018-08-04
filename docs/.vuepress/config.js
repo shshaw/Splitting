86b152cb8364183785669a1b7ef013bcffe82fc6
Christopher Wallis <christopher.j.wallis@gmail.com>
2018-08-04 19:42:22 -0400

    changed config to point to splitting

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: "Splitting",
     description: "CSS Vars for split words, chars, and images!",
-    base: "/splitting-docs/",
+    base: "/splitting/",
     head: [
         // prettier-ignore
         ["link", { rel: "icon", href: "/favicon.png" }],
@@ -17,11 +17,11 @@ module.exports = {
         repoLabel: "Contribute!",
 
         // if your docs are in a different repo from your main project:
-        docsRepo: "shshaw/splitting-docs",
+        docsRepo: "shshaw/splitting",
         // if your docs are not at the root of the repo:
         docsDir: "docs",
         // if your docs are in a specific branch (defaults to 'master'):
-        docsBranch: "source",
+        docsBranch: "docs",
         // defaults to false, set to true to enable
         editLinks: true,
         // custom text for edit link. Defaults to "Edit this page"
