0fbeae00b2e489fab010bf5c02f36f61e3c7e936
Stephen Shaw <shshaw@gmail.com>
2018-08-04 22:29:07 -0500

    Updated base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,34 @@
 module.exports = {
     title: "Splitting",
     description: "CSS Vars for split words, chars, and images!",
-    base: "/splitting/",
+    base: "/",
     head: [
         // prettier-ignore
-        ["link", { rel: "icon", href: "/favicon.png" }],
-        ["link", { rel: 'stylesheet', href: "https://fonts.googleapis.com/css?family=Kanit:900i|Raleway:400" }],
-        ["link", { rel: 'stylesheet', href: "https://lucid-northcutt-877344.netlify.com/dist/splitting.css" }],
-        ["link", { rel: 'stylesheet', href: "https://lucid-northcutt-877344.netlify.com/dist/splitting-cells.css" }],
-        ["script", { src: "https://lucid-northcutt-877344.netlify.com/dist/splitting.js" }]
+        ["link", {
+            rel: "icon",
+            href: "/favicon.png"
+        }],
+        ["link", {
+            rel: 'stylesheet',
+            href: "https://fonts.googleapis.com/css?family=Kanit:900i|Raleway:400"
+        }],
+        ["link", {
+            rel: 'stylesheet',
+            href: "https://lucid-northcutt-877344.netlify.com/dist/splitting.css"
+        }],
+        ["link", {
+            rel: 'stylesheet',
+            href: "https://lucid-northcutt-877344.netlify.com/dist/splitting-cells.css"
+        }],
+        ["script", {
+            src: "https://lucid-northcutt-877344.netlify.com/dist/splitting.js"
+        }]
     ],
     themeConfig: {
-        nav: [{ text: "Guide", link: "/guide.md" }],
+        nav: [{
+            text: "Guide",
+            link: "/guide.md"
+        }],
         // Assumes GitHub. Can also be a full GitLab url.
         repo: "shshaw/splitting",
         repoLabel: "Contribute!",
